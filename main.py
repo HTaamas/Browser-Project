1117a1e71f5ba5cc8dd5640a8d973ae467879209
HTaamas <96864185+HTaamas@users.noreply.github.com>
2024-02-29 09:11:15 +0100

    removed comments

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ class DownloadHistoryListWidget(QListWidget):
 
     def contextMenuEvent(self, event):
         menu = QMenu(self)
-        open_file_action = menu.addAction("Open file")  # New action
-        open_file_location_action = menu.addAction("Open files location")  # New action
+        open_file_action = menu.addAction("Open file")
+        open_file_location_action = menu.addAction("Open files location")
         delete_action = menu.addAction("Delete")
-        delete_file_action = menu.addAction("Delete File")  # New action
+        delete_file_action = menu.addAction("Delete File")
         action = menu.exec_(self.mapToGlobal(event.pos()))
         current_item = self.currentItem()
         if current_item is not None:
@@ -40,33 +40,30 @@ class DownloadHistoryListWidget(QListWidget):
             item_text = current_item.text()
             if action == delete_action:
                 self.takeItem(row)
-                # Remove the 'X ' prefix from the item_text if it exists
                 item_text = item_text[2:] if item_text.startswith('X ') else item_text
                 if item_text in self.browser_window.download_history:
                     self.browser_window.download_history.remove(item_text)
-                    self.browser_window.browser_window.save_download_history()  # Save the download history
-            elif action == delete_file_action:  # New condition
+                    self.browser_window.browser_window.save_download_history()
+            elif action == delete_file_action:
                 reply = QMessageBox.question(self, 'Delete Confirmation',
                                              "Are you sure you want to delete the file?",
                                              QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
                 if reply == QMessageBox.Yes:
-                    # If user clicked 'Yes', delete the file
-                    # Remove the 'X ' prefix from the item_text if it exists
                     item_text = item_text[2:] if item_text.startswith('X ') else item_text
                     if os.path.exists(item_text):
                         os.remove(item_text)
                         if item_text in self.browser_window.download_history:
                             self.browser_window.download_history.remove(item_text)
-                            self.browser_window.save_download_history()  # Save the download history
-                            self.takeItem(row)  # Remove the item from the list
-            elif action == open_file_location_action:  # New condition
+                            self.browser_window.save_download_history()
+                            self.takeItem(row)
+            elif action == open_file_location_action:
                 item_text = item_text[2:] if item_text.startswith('X ') else item_text
                 if os.path.exists(item_text):
-                    subprocess.Popen(f'explorer /select,"{os.path.abspath(item_text)}"')  # Open file location and select the file  # Open file location
-            elif action == open_file_action:  # New condition
+                    subprocess.Popen(f'explorer /select,"{os.path.abspath(item_text)}"')
+            elif action == open_file_action:
                 item_text = item_text[2:] if item_text.startswith('X ') else item_text
                 if os.path.exists(item_text):
-                    os.startfile(item_text)  # Open the file
+                    os.startfile(item_text)
 
 class DownloadHistoryWindow(QMainWindow):
     def __init__(self, download_history, browser_window):
@@ -77,7 +74,6 @@ class DownloadHistoryWindow(QMainWindow):
         self.setGeometry(100, 100, 400, 300)
         self.setStyleSheet("background-color: #222; color: #eee;")
 
-        # Create a QVBoxLayout instance
         self.main_layout = QVBoxLayout()
 
         if download_history:
@@ -87,20 +83,17 @@ class DownloadHistoryWindow(QMainWindow):
                 if not os.path.exists(path):
                     path = 'X ' + path
                 self.list_widget.addItem(path)
-            self.main_layout.addWidget(self.list_widget)  # Add the list_widget to the layout
-            # Create "Clear All" button
+            self.main_layout.addWidget(self.list_widget)
             self.clear_all_button = QPushButton("Clear All")
             self.clear_all_button.clicked.connect(self.clear_all)
-            self.main_layout.addWidget(self.clear_all_button)  # Add the button to the layout
+            self.main_layout.addWidget(self.clear_all_button)
         else:
             self.label = QLabel("There aren't any downloads to display", self)
             self.label.setAlignment(Qt.AlignCenter)
-            self.main_layout.addWidget(self.label)  # Add the label to the layout
+            self.main_layout.addWidget(self.label)
 
-        # Create a QWidget instance and set it as the central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        # Set the main_layout as the layout for the central widget
         central_widget.setLayout(self.main_layout)
 
     def clear_all(self):
@@ -138,7 +131,6 @@ class HistoryWindow(QMainWindow):
         self.setGeometry(100, 100, 400, 300)
         self.setStyleSheet("background-color: #222; color: #eee;")
 
-        # Create a QVBoxLayout instance
         self.main_layout = QVBoxLayout()
 
         if history:
@@ -146,20 +138,17 @@ class HistoryWindow(QMainWindow):
             self.list_widget.setStyleSheet("background-color: #333; color: #eee;")
             for url in history:
                 self.list_widget.addItem(url)
-            self.main_layout.addWidget(self.list_widget)  # Add the list_widget to the layout
-            # Create "Clear All" button
+            self.main_layout.addWidget(self.list_widget)
             self.clear_all_button = QPushButton("Clear All")
             self.clear_all_button.clicked.connect(self.clear_all)
-            self.main_layout.addWidget(self.clear_all_button)  # Add the button to the layout
+            self.main_layout.addWidget(self.clear_all_button)
         else:
             self.label = QLabel("There aren't any history to display", self)
             self.label.setAlignment(Qt.AlignCenter)
-            self.main_layout.addWidget(self.label)  # Add the label to the layout
+            self.main_layout.addWidget(self.label)
 
-        # Create a QWidget instance and set it as the central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        # Set the main_layout as the layout for the central widget
         central_widget.setLayout(self.main_layout)
 
     def clear_all(self):
@@ -178,12 +167,10 @@ class CloseableTabWidget(QTabWidget):
         self.tabCloseRequested.connect(self.emit_tab_close_request)
         self.user_made_change = False
 
-        # Create hamburger menu button
         self.hamburger_button = QPushButton("☰")
         self.hamburger_button.setStyleSheet("background-color: #333; color: #eee; width: 24px; height: 24px;")
         self.hamburger_button.setContentsMargins(0, 0, 0, 0)
-        self.hamburger_button.clicked.connect(
-            self.show_hamburger_menu)  # Connect clicked signal to show_hamburger_menu method
+        self.hamburger_button.clicked.connect(self.show_hamburger_menu)
         self.setCornerWidget(self.hamburger_button)
 
         self.tabBar().setTabButton(0, QTabBar.RightSide, self.hamburger_button)
@@ -224,20 +211,16 @@ class CloseableTabWidget(QTabWidget):
         self.setStyleSheet("QTabBar::tab { color: black; }")
 
     def show_hamburger_menu(self):
-        # Create context menu
         hamburger_menu = QMenu(self)
 
-        # Create "Browsing History" action
         browsing_history_action = QAction("Browsing History", self)
         browsing_history_action.triggered.connect(self.browser_window.show_history)
         hamburger_menu.addAction(browsing_history_action)
 
-        # Create "Download History" action
         download_history_action = QAction("Download History", self)
         download_history_action.triggered.connect(self.browser_window.show_download_history)
         hamburger_menu.addAction(download_history_action)
 
-        # Show context menu
         hamburger_menu.exec_(QCursor.pos())
 
     def emit_tab_close_request(self, index):
@@ -263,7 +246,6 @@ class CloseableTabWidget(QTabWidget):
                 search_query = text.strip()
                 url = f"https://www.google.com/search?q={'+'.join(search_query.split())}"
                 web_engine_page.load(QUrl(url))
-            # url_bar.setCursorPosition(0)
 
 
 class BrowserWindow(QMainWindow):
@@ -286,7 +268,6 @@ class BrowserWindow(QMainWindow):
         self.tab_widget.create_tab()
         self.tab_widget.tabCloseRequestedCustom.connect(self.close_tab)
 
-        # Set up download handler
         QWebEngineProfile.defaultProfile().downloadRequested.connect(self.handle_download)
 
         self.history = []
@@ -299,14 +280,11 @@ class BrowserWindow(QMainWindow):
             self.close()
 
     def handle_download(self, download):
-        # Prompt user to select download location
         path, _ = QFileDialog.getSaveFileName(self, "Save File", download.path())
         if path:
-            # Extract file extension from original URL
             original_url = download.url().toString()
             parsed_url = urlparse(original_url)
             file_extension = os.path.splitext(parsed_url.path)[1]
-            # Append file extension to path
             path += file_extension
             download.setPath(path)
             download.accept()
@@ -314,32 +292,26 @@ class BrowserWindow(QMainWindow):
         self.save_download_history()
 
     def closeEvent(self, event):
-        # Save history and download history when application is about to close
         self.save_history()
         event.accept()
 
     def save_history(self):
-        # Create directory if it doesn't exist
         directory = os.path.expanduser('~\\AppData\\Roaming\\htaamas\\browser')
         os.makedirs(directory, exist_ok=True)
 
-        # Save browsing history to a file
         with open(os.path.join(directory, 'history.json'), 'w') as f:
             json.dump(self.history, f)
 
     def load_history(self):
-        # Create directory if it doesn't exist
         directory = os.path.expanduser('~\\AppData\\Roaming\\htaamas\\browser')
         os.makedirs(directory, exist_ok=True)
 
-        # Load browsing history from a file
         try:
             with open(os.path.join(directory, 'history.json'), 'r') as f:
                 self.history = json.load(f)
         except FileNotFoundError:
             self.history = []
 
-        # Load download history from a file
         try:
             with open(os.path.join(directory, 'download_history.json'), 'r') as f:
                 self.download_history = json.load(f)
@@ -355,11 +327,9 @@ class BrowserWindow(QMainWindow):
         self.history_window.show()
 
     def save_download_history(self):
-        # Create directory if it doesn't exist
         directory = os.path.expanduser('~\\AppData\\Roaming\\htaamas\\browser')
         os.makedirs(directory, exist_ok=True)
 
-        # Save download history to a file
         with open(os.path.join(directory, 'download_history.json'), 'w') as f:
             json.dump(self.download_history, f)
 
